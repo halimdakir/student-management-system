3effd9586dc8878c41a196723b971ce654ed9e32
Halimove01 <halim.dakir@gmail.com>
2020-11-07 19:34:35 +0100

    fix crud fonctions on student service

--- src/main/java/se/iths/service/StudentService.java
+++ src/main/java/se/iths/service/StudentService.java
@@ -3,11 +3,13 @@ package se.iths.service;
 import se.iths.entity.Student;
 import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
-
+@Transactional
 public class StudentService {
 
-
+    @PersistenceContext
     EntityManager entityManager;
 
     public Student createStudent(Student student) {
@@ -20,12 +22,16 @@ public class StudentService {
         return student;
     }
 
-    public Student findStudentById(Long id) {
-        return entityManager.find(Student.class, id);
+    public Student findStudentById(String lastname) {
+        return entityManager.find(Student.class, lastname);
     }
 
     public List<Student> getAllStudents() {
         return entityManager.createQuery("SELECT s from Student s", Student.class).getResultList();
+    }
+
+    public void deleteStudent(String lastname){
+        entityManager.remove(entityManager.find(Student.class, lastname));
     }
 
 }
