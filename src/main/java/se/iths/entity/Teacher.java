f16dca7c79a6a476b5dbf0df1dc95d1a5812ebe9
Halimove01 <halim.dakir@gmail.com>
2020-11-18 15:31:19 +0100

    add getter and setter and consructors to teacher entity

--- src/main/java/se/iths/entity/Teacher.java
+++ src/main/java/se/iths/entity/Teacher.java
@@ -10,5 +10,60 @@ import javax.validation.constraints.Pattern;
 
 @Entity
 public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotEmpty(message = "Required")
+    private String firstName;
+    @NotEmpty(message = "Required")
+    private String lastName;
+    @NotNull(message = "Required")
+    @Pattern(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "Exemple@email.com")
+    private String email;
+    @Pattern(regexp = "^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\\s\\./0-9]*$", message = "0700 000000")
+    private String phoneNumber;
 
+    public Teacher() {
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
