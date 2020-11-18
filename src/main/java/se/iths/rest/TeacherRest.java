16b2073b36c56fa1285aade87469144a67fa65d4
Halimove01 <halim.dakir@gmail.com>
2020-11-18 17:58:14 +0100

    created teacher rest

--- src/main/java/se/iths/rest/TeacherRest.java
+++ src/main/java/se/iths/rest/TeacherRest.java
@@ -1,12 +1,38 @@
 package se.iths.rest;
 
+import se.iths.annotation.CorrectNameFormat;
+import se.iths.annotation.NameProcessor;
+import se.iths.entity.Subject;
+import se.iths.entity.Teacher;
+import se.iths.service.SubjectService;
+import se.iths.service.TeacherService;
+
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("teacher")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class TeacherRest {
+
+
+    @Inject
+    TeacherService teacherService;
+    NameProcessor nameProcessor;
+
+    @Inject
+    public TeacherRest(@CorrectNameFormat NameProcessor nameProcessor) {
+        this.nameProcessor = nameProcessor;
+    }
+
+    @Path("all")
+    @GET
+    public List<Teacher> getAll(){
+        return teacherService.getAllTeachers();
+    }
 }
